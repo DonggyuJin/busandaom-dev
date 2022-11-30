6b06745dbf5a7eea2fcbf8cbe948bdffcf919d39
jdg70 <jdk2531@gmail.com>
2022-11-30 19:49:34 +0900

    fixed untracked files

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "busandaomweb": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
